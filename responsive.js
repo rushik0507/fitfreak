9fdac1bbe4d197c823957cf0efeb47a2334683b9
rushik0507 <rushikpateljnv@gmail.com>
2023-11-01 01:04:49 +0530

    added the button icons for closing the sections

--- responsive.js
+++ responsive.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".rev-button");
 const nav = document.querySelector(".sidenav");
 const revnav = document.getElementById("res");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
 
 var count = 0;
 
@@ -8,10 +10,14 @@ button.addEventListener("click", () => {
   if (count % 2 == 0) {
     nav.style.display = "block";
     revnav.style.backgroundColor = "transparent";
+    open.style.display = "none";
+    close.style.display = "block";
     count++;
   } else {
     nav.style.display = "none";
     revnav.style.backgroundColor = "transparent";
+    open.style.display = "block";
+    close.style.display = "none";
     count++;
   }
 });
@@ -19,10 +25,22 @@ button.addEventListener("click", () => {
 const btn = document.querySelector(".sidebar-button");
 const sidebar = document.querySelector(".sidebar");
 const sectionside = document.querySelector(".side-section");
+const sideclose = document.getElementById("sideclose");
+const ssection = document.querySelector(".side-section");
 
 function toggleMenu() {
   sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
 }
+
+ssection.addEventListener("click", function () {
+  sidebar.style.display = "none";
+  btn.style.display = "block";
+});
+
+sideclose.addEventListener("click", function () {
+  sidebar.style.display = "none";
+  btn.style.display = "block";
+});
 
 // Event listener for the button click
 btn.addEventListener("click", function (event) {
